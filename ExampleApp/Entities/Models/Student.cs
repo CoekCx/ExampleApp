e71f14b33479b58edcd4a998338612340ff1fff0
CoekCx <nemanja.ja.jovicic7@gmail.com>
2024-10-29 22:58:33 +0100

    added student model

---
+++ ExampleApp/Entities/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using ExampleApp.Entities.Abstractions;
+
+namespace ExampleApp.Entities.Models;
+
+internal class Student(string firstName, string lastName) : IStudent
+{
+    public string FirstName { get; set; } = firstName;
+    public string LastName { get; set; } = lastName;
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi I am {FirstName} {LastName}.");
+    }
+}
